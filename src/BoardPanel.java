263065d5dbd472cf65f92c751c38189b219c9119
Lynxi <93663504+Lynxions@users.noreply.github.com>
2023-12-19 21:45:59 +0700

    Update BoardPanel.java

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -54,5 +54,55 @@ public class BoardPanel extends JPanel {
       }
     }
     
+    g.setColor(Color.DARK_GRAY);
+		g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+		for(int x = 0; x < COLUMN; x++) {
+			for(int y = 0; y < ROW; y++) {
+				g.drawLine(x * TILE_SIZE, 0, x * TILE_SIZE, getHeight());
+				g.drawLine(0, y * TILE_SIZE, getWidth(), y * TILE_SIZE);
+			}
+		}
+
+    if(game.isGameOver() || game.isNewGame() || game.isPaused()) {
+      g.setColor(Color.WHITE);
+
+      int centerX = getWidth() / 2;
+      int centerY = getHeight() / 2;
+
+      String largeMessage = null;
+			String smallMessage = null;
+			if(game.isNewGame()) {
+				largeMessage = "Snake Game!";
+				smallMessage = "Press Enter to Start";
+			} else if(game.isGameOver()) {
+				largeMessage = "Game Over!";
+				smallMessage = "Press Enter to Restart";
+			} else if(game.isPaused()) {
+				largeMessage = "Paused";
+				smallMessage = "Press P to Resume";
+			}
+
+      g.setFont(FONT);
+			g.drawString(largeMessage, centerX - g.getFontMetrics().stringWidth(largeMessage) / 2, centerY - 50);
+			g.drawString(smallMessage, centerX - g.getFontMetrics().stringWidth(smallMessage) / 2, centerY + 50);
+    }
   }
+
+  private void drawTile(int x, int y, TileType type, Graphics g) {
+    switch(type) {
+      case Fruit:
+        g.setColor(Color.RED);
+        g.fillOval(x + 2, y + 2, TILE_SIZE - 4, TILE_SIZE - 4);
+			  break;
+
+      case SnakeBody:
+        g.setColor(Color.GREEN);
+			  g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
+			  break;
+        
+      case SnakeHead:
+        g.setColor(Color.GREEN);
+        g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
+    }
+  }  
 }
